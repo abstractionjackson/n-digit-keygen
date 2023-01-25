0bdff62433553dfe981a211d4372dd3d0ed8e4be
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-01-24 21:45:47 -0800

    handle select width on length n, use custom dropdown arrow

--- src/lib/components/Welcome/index.svelte
+++ src/lib/components/Welcome/index.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { code as keycode } from "../../../stores";
   import { getStringOfNRandDigits } from "../../utils";
+  import chevronDown from "../../../assets/chevron_down.svg";
 
-  let selected: number = 6;
+  let n: number = 6;
+  $: selectWidth = n.toString().length === 1 ? "1.25em" : "1.75em";
 
-  const handleChange = (e: Event) => {
-    //@ts-ignore
-    const n = e.target.value;
+  const handleChange = () => {
     const code = getStringOfNRandDigits(n);
     keycode.set(code);
   };
@@ -17,18 +17,34 @@
   class="flex min-h-[50vh] w-full flex-col justify-end bg-gradient-to-t from-neutral-300 to-neutral-400 py-8 text-center text-black text-opacity-90"
 >
   <h1 class="text-4xl font-bold">
-    <select
-      bind:value={selected}
-      on:change={handleChange}
-      class="inline w-min bg-transparent"
+    <div
+      class="arrow"
+      style="width: {selectWidth}; background-image: url({chevronDown});"
     >
-      {#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as i}
-        {#if i === selected}
-          <option value={i} selected>{i}</option>
-        {:else}
-          <option value={i}>{i}</option>
-        {/if}
-      {/each}
-    </select> Digit Keygen
+      <select
+        bind:value={n}
+        on:change={handleChange}
+        class=" appearance-none bg-transparent text-red-600 underline"
+        style="width: {selectWidth}"
+      >
+        {#each [4, 5, 6, 7, 8, 9, 10] as i}
+          {#if i === n}
+            <option value={i} selected>{i}</option>
+          {:else}
+            <option value={i}>{i}</option>
+          {/if}
+        {/each}
+      </select>
+    </div>
+    Digit KeyGen
   </h1>
 </div>
+
+<style>
+  .arrow {
+    background-repeat: no-repeat;
+    display: inline;
+    background-position: bottom 3px right -3px;
+    background-size: 24px;
+  }
+</style>
