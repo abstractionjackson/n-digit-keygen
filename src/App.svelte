6f8c191416ae78025d06faec7c94bd4815398e9f
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-01-24 20:10:52 -0800

    add custsom og tags

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,16 @@
 <svelte:head>
   <!-- favicon -->
   <link rel="icon" type="image/png" href={favicon} />
-  <title>N-Digit Keygen</title>
+  <title>N-Digit KeyGen</title>
+  <meta
+    property="og:image"
+    content="https://media.giphy.com/media/Ps8XflhsT5EVa/giphy.gif"
+  />
+  <meta
+    property="og:description"
+    content="Get N Random Digits: Now in lengths 4, 5, 6, 7, and more!"
+  />
+  <meta property="og:title" content="N-Digit KeyGen" />
 </svelte:head>
 
 <main class="min-h-screen bg-neutral-300">
